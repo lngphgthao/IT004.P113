bb2b74133c83be8454930165c0587ff05ed74a76
Le Ngoc Phuong Thao <phuongthao27102005@gmail.com>
2024-09-29 13:02:08 +0700

    Upload Lab01-Homework folder

---
+++ Lab01-Homework/sql-questions-it-cs-beginner-exercise.sql
@@ -0,0 +1,64 @@
+-- 25 câu truy vấn cơ bản mới cho người mới bắt đầu, không sử dụng JOIN (51-75)
+
+-- 51. Hiển thị tất cả thông tin của bảng ChuyenGia.
+
+
+-- 52. Liệt kê họ tên và email của tất cả chuyên gia.
+
+-- 53. Hiển thị tên công ty và số nhân viên của tất cả các công ty.
+
+-- 54. Liệt kê tên các dự án đang trong trạng thái 'Đang thực hiện'.
+
+
+-- 55. Hiển thị tên và loại của tất cả các kỹ năng.
+
+-- 56. Liệt kê họ tên và chuyên ngành của các chuyên gia nam.
+
+
+-- 57. Hiển thị tên công ty và lĩnh vực của các công ty có trên 150 nhân viên.
+
+-- 58. Liệt kê tên các dự án bắt đầu trong năm 2023.
+
+-- 59. Hiển thị tên kỹ năng thuộc loại 'Công cụ'.
+
+-- 60. Liệt kê họ tên và số năm kinh nghiệm của các chuyên gia có trên 5 năm kinh nghiệm.
+
+
+-- 61. Hiển thị tên công ty và địa chỉ của các công ty trong lĩnh vực 'Phát triển phần mềm'.
+
+-- 62. Liệt kê tên các dự án có ngày kết thúc trong năm 2023.
+
+
+-- 63. Hiển thị tên và cấp độ của các kỹ năng trong bảng ChuyenGia_KyNang.
+
+-- 64. Liệt kê mã chuyên gia và vai trò trong các dự án từ bảng ChuyenGia_DuAn.
+
+
+-- 65. Hiển thị họ tên và ngày sinh của các chuyên gia sinh năm 1990 trở về sau.
+
+-- 66. Liệt kê tên công ty và số nhân viên, sắp xếp theo số nhân viên giảm dần.
+
+-- 67. Hiển thị tên dự án và ngày bắt đầu, sắp xếp theo ngày bắt đầu tăng dần.
+
+-- 68. Liệt kê tên kỹ năng, chỉ hiển thị mỗi tên một lần (loại bỏ trùng lặp).
+
+-- 69. Hiển thị họ tên và email của 5 chuyên gia đầu tiên trong danh sách.
+
+
+-- 70. Liệt kê tên công ty có chứa từ 'Tech' trong tên.
+
+
+-- 71. Hiển thị tên dự án và trạng thái, không bao gồm các dự án đã hoàn thành.
+
+
+-- 72. Liệt kê họ tên và chuyên ngành của các chuyên gia, sắp xếp theo chuyên ngành và họ tên.
+
+
+-- 73. Hiển thị tên công ty và lĩnh vực, chỉ bao gồm các công ty có từ 100 đến 200 nhân viên.
+
+
+-- 74. Liệt kê tên kỹ năng và loại kỹ năng, sắp xếp theo loại kỹ năng giảm dần và tên kỹ năng tăng dần.
+
+
+-- 75. Hiển thị họ tên và số điện thoại của các chuyên gia có email sử dụng tên miền 'email.com'.
+
